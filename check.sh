aa664fdd324868e15639171c9695c2b6bc8f4b1a
Anna van Biemen <annavanbiemen@gmail.com>
2025-07-19 13:33:54 +0200

    Improve lint/check script.

--- check.sh
+++ check.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+# Exit on errors
 set -euo pipefail
 
-cd "$( dirname "$0")"
+# Change directory to the setup home
+cd "$( dirname "$0" )"
+
+# Lint scripts using bash (and POSIC-compatible dash in case of `env`)
+bin/require-apt dash bash
+dash -n env
+find bin lib check.sh demo.sh install.sh env -type f -exec bash -x -n {} +
+
+# Run shellcheck on all scripts
 bin/require-apt shellcheck
-find bin etc check.sh demo.sh install.sh -type f -executable -exec shellcheck -x {} +
+find bin lib check.sh demo.sh install.sh env -type f -exec shellcheck -x {} +
+
+# Check formatting of the justfile
+bin/require-apt just
 just --check --fmt --unstable
+
+# Done!
 echo "All OK!"
