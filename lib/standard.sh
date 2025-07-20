2df0b3341d112edce58b5c1f6bd2d5b31584cb57
Anna van Biemen <annavanbiemen@gmail.com>
2025-07-20 16:51:24 +0200

    Refactor libraries

--- lib/standard.sh
+++ lib/standard.sh
@@ -1,43 +1,49 @@
-#!/bin/bash
+# shellcheck shell=bash
 
-# Exit on errors
-set -euo pipefail
+# Setup main library by Anna van Biemen
+#
+# Functions:
+#   standard::error [message]
+#   standard::raise [command]
+#   standard::trace
+#   standard::debug
+#   standard::with <option>
+#   standard::usage <function>
+#   standard::version
 
-# Import module
-import() {
-    local module="$1"
 
-    # shellcheck source=/dev/null
-    source "$( dirname "${BASH_SOURCE[0]}" )/${module}"
+# Output an error
+#
+# Usage: standard::error [message]
+#
+# Arguments:
+#   message  Message text
+standard::error() {
+    local text="$1"
+
+    printf "ERROR: %s" "$text" >&2
 }
 
-# Import functions from module
-from() {
-    local module="$1"
-    local operation="$2"
-    local functions=("${@:3}")
-
-    [[ "${#functions[@]}" -gt 0 ]] || raise echo "from: No function arguments given"
-    [[ "$operation" == "import" ]] || raise echo "from: Second argument must be 'import'"
-
-    local function
-    import "$module"
-    for function in "${functions[@]}"; do
-        eval "function ${function} { ${module}::${function} \"\$@\"; }"
-    done
-}
-
-# Raise code 1 and optionally send output from a command to STDERR
-raise() {
+# Raise non-zero exit code and output from optional command to STDERR
+#
+# Usage: standard::raise [command]
+#
+# Arguments:
+#   command  Optional command to run, output gets redirected to STDERR
+#
+# Returns non-zero exit code returned from command or 1 otherwise
+standard::raise() {
     if [[ "$#" -gt 0 ]]; then
-        "$@" >&2
+        "$@" >&2 || return $?
     fi
 
     return 1
 }
 
 # Print trace including arguments (if extdebug was enabled)
-trace() {
+#
+# Usage: standard::trace
+standard::trace() {
     printf "\n\e[4mTrace\e[0m\n"
     local frame arg arg_count arg_pos=${BASH_ARGC[0]} file line call
     for ((frame = 1; frame < ${#FUNCNAME[@]}; frame++)); do
@@ -56,13 +62,21 @@ trace() {
 }
 
 # Enable extdebug to keep track of arguments and handle ERR exit using a trace
-debug() {
+#
+# Usage: standard::debug
+standard::debug() {
     shopt -s extdebug
-    trap trace ERR
+    trap standard::trace ERR
 }
 
 # Run command with option enabled
-with() {
+#
+# Usage: standard::with <option> <command>
+#
+# Arguments:
+#   option   Option to enable (temporarily) while executing the command
+#   command  Command to run while the option is enabled
+standard::with() {
     local option="$1"
     shift
 
@@ -71,21 +85,28 @@ with() {
         return $?
     fi
 
+    local exit=0
     shopt -s "$option" > /dev/null
-    "$@"
-    local exit=$?
+    "$@" || exit=$?
     shopt -u "$option" > /dev/null
 
     return $exit
 }
 
-# Show usage information
-usage() {
+# Show usage information for a function
+#
+# Usage: standard::usage [function]
+#
+# Arguments
+#   function  Function to display usage for
+standard::usage() {
+    # Read arguments
+    [[ $# -le 1 ]] || raise usage
     local function="${1:-main}"
 
     # Determine file where function was defined
     local info file
-    IFS=" " read -r -a info <<< "$(with extdebug declare -F "$function")"
+    IFS=" " read -r -a info <<< "$( standard::with extdebug declare -F "$function" )"
     file="${info[2]}"
 
     # Use awk to parse the usage information
@@ -110,6 +131,8 @@ usage() {
 }
 
 # Show version information
-version() {
+#
+# Usage: standard::version
+standard::version() {
     git -C "$( dirname "$0" )" describe --always --dirty
 }
