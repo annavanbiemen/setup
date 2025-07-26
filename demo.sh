fd2dbdccdb770f9fa98bce1127d4125b6a04edf8
Anna van Biemen <annavanbiemen@gmail.com>
2025-07-26 15:41:19 +0200

    Add script comments.

--- demo.sh
+++ demo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Exit on errors
 set -euo pipefail
 
+# Build the setup image
 docker build --build-arg TZ="$( cat /etc/timezone )" --tag setup .
+
+# Run an interactive bash shell in a container using the setup image
 docker run -it --rm --env "TERM=${TERM}" --hostname sandbox setup
