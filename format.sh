28c387b61e9a1771ddda101c20cde3fb98ef5b99
Anna van Biemen <annavanbiemen@gmail.com>
2026-02-01 18:36:33 +0100

    Add justfile to format.sh

--- format.sh
+++ format.sh
@@ -8,4 +8,11 @@ cd "$(dirname "$0")"
 
 # Format shell scripts using shfmt
 bin/require-apt shfmt
-shfmt --indent 4 --space-redirects --write ./*.sh env bin/* lib/*.sh
+shfmt --indent 4 --space-redirects --write ./*.sh bin/* lib/*.sh env
+
+# Format justfile
+bin/require-apt just
+just --fmt --unstable
+
+# Done!
+echo "Done!"
