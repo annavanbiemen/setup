3fae9b4c9ccfe23c75230f6deab5718b26be8987
Anna van Biemen <annavanbiemen@gmail.com>
2026-02-01 18:31:48 +0100

    Fix check.sh to include other .sh scripts

--- check.sh
+++ check.sh
@@ -9,11 +9,11 @@ cd "$(dirname "$0")"
 # Lint scripts using bash (and POSIX-compatible dash in case of `env`)
 bin/require-apt dash bash
 dash -n env
-find bin lib/*.sh check.sh demo.sh install.sh env -type f -exec bash -x -n {} +
+find ./*.sh bin/* lib/*.sh env -type f -exec bash -x -n {} +
 
 # Run shellcheck on all scripts
 bin/require-apt shellcheck
-find bin lib/*.sh check.sh demo.sh install.sh env justfile -type f -exec shellcheck -x {} +
+find ./*.sh bin/* lib/*.sh env justfile -type f -exec shellcheck -x {} +
 
 # Check shell script formatting using shfmt
 bin/require-apt shfmt
--- test.sh
+++ test.sh
@@ -15,7 +15,7 @@ docker build --build-arg TZ="$(cat /etc/timezone)" --tag setup .
 echo "Testing recipes:"
 for recipe in $(just --summary); do
     printf -- "- %-10s" "${recipe}"
-    if docker run -it --rm --env "TERM=${TERM}" --hostname sandbox setup setup/bin/setup "$recipe" &> "log/$recipe.log"; then
+    if docker run -it --rm --env "TERM=${TERM}" --hostname sandbox setup setup/bin/setup "${recipe}" &> "log/${recipe}.log"; then
         printf "✅ "
     else
         printf "❌ "
