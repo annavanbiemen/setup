a67617c123bcc670315336842ad474c17d3d5485
Anna van Biemen <annavanbiemen@gmail.com>
2026-02-03 00:56:17 +0100

    Fix test.sh

--- test.sh
+++ test.sh
@@ -13,9 +13,9 @@ mkdir -p log
 docker build --build-arg TZ="$(cat /etc/timezone)" --tag setup .
 
 echo "Testing recipes:"
-for recipe in $(docker run --rm setup setup/bin/setup --summary); do
+for recipe in $(docker run --rm setup setup/bin/setup summary); do
     printf -- "- %-10s" "${recipe}"
-    if docker run -it --rm --env "TERM=${TERM}" --hostname sandbox setup setup/bin/setup "${recipe}" &> "log/${recipe}.log"; then
+    if docker run -it --rm --env "TERM=${TERM}" --hostname sandbox setup setup/bin/setup add "${recipe}" &> "log/${recipe}.log"; then
         printf "✅ "
     else
         printf "❌ "
