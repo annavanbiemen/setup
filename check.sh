f59adb3b4cdd907350ca1fbbfb0db5f149a36ccb
Anna van Biemen <annavanbiemen@gmail.com>
2026-02-03 01:13:34 +0100

    Fix check.sh

--- check.sh
+++ check.sh
@@ -9,17 +9,11 @@ cd "$(dirname "$0")"
 # Lint scripts using bash (and POSIX-compatible dash in case of `env`)
 bin/require-apt dash bash
 dash -n env
-find . -maxdepth 1 -name '*.sh' -type f -exec bash -x -n {} +
-find bin -type f -exec bash -x -n {} +
-find lib -name '*.sh' -type f -exec bash -x -n {} +
-bash -x -n env
+find . -path './.git' -prune -o -type f \( -executable -o -name "*.sh" -o -path './env' \) -exec bash -x -n {} +
 
 # Run shellcheck on all scripts
 bin/require-apt shellcheck
-find . -maxdepth 1 -name '*.sh' -type f -exec shellcheck -x {} +
-find bin -type f -exec shellcheck -x {} +
-find lib -name '*.sh' -type f -exec shellcheck -x {} +
-shellcheck -x env
+find . -path './.git' -prune -o -type f \( -executable -o -name "*.sh" -o -path './env' \) -exec shellcheck -x {} +
 
 # Check shell script formatting using shfmt
 bin/require-apt shfmt
