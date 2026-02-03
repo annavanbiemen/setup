b0bde2bd754d38ccc8107363b856d803d3537b95
Anna van Biemen <annavanbiemen@gmail.com>
2026-02-03 01:43:00 +0100

    Move append function to config::add

---
+++ lib/config.sh
@@ -0,0 +1,45 @@
+# shellcheck shell=bash
+
+# Add lines to configuration file (only if not already present)
+#
+# Usage: config::add <file> <line> [<line> ...]
+#
+# Arguments:
+#   file  File to append to
+#   line  Lines to add (unless already present) one by one
+config::has() {
+    # Read arguments
+    local file="$1"
+    local line="$2"
+
+    if [[ -f "${file}" ]] && grep -qxF "${line}" "${file}"; then
+        return 0 # true
+    fi
+
+    return 1 # false
+}
+
+# Add lines to configuration file (only if not already present)
+#
+# Usage: config::add <file> <line> [<line> ...]
+#
+# Arguments:
+#   file      File to append to
+#   line ...  Lines to add (unless already present) one by one
+config::add() {
+    # Read arguments
+    local file="$1"
+    local lines=("${@:2}")
+
+    # Iterate over given lines
+    local line
+    for line in "${lines[@]}"; do
+        # Skip line if already present
+        if config::has "${file}" "${line}"; then
+            continue
+        fi
+
+        # Add line to file
+        echo "${line}" >> "${file}"
+    done
+}
